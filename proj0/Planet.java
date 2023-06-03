780d21243e09e434bc1d9613c4a9e0340e08413e
yicyuanliang <gnr1118@gmail.com>
2023-06-03 15:48:24 +0800

    [proj0] add calculate methods to Planet.java

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,4 +1,6 @@
 public class Planet {
+    public static final double G = 6.67e-11;
+
     public double xxPos;
     public double yyPos;
     public double xxVel;
@@ -23,5 +25,61 @@ public class Planet {
         this.yyVel = planet.yyVel;
         this.mass = planet.mass;
         this.imgFileName = planet.imgFileName;
+    }
+
+    public double calcDistance(Planet planet) {
+        double dx = planet.xxPos - this.xxPos;
+        double dy = planet.yyPos - this.yyPos;
+        return Math.pow(dx * dx + dy * dy, 0.5);
+    }
+
+    public double calcForceExertedBy(Planet planet) {
+        double r = calcDistance(planet);
+        return (G * this.mass * planet.mass) / (r * r);
+    }
+
+    public double calcForceExertedByX(Planet planet) {
+        double dx = planet.xxPos - this.xxPos;
+        double r = calcDistance(planet);
+        return calcForceExertedBy(planet) * dx / r;
+    }
+
+    public double calcForceExertedByY(Planet planet) {
+        double dy = planet.yyPos - this.yyPos;
+        double r = calcDistance(planet);
+        return calcForceExertedBy(planet) * dy / r;
+    }
+
+    public double calcNetForceExertedByX(Planet[] planets) {
+        double sum = 0.0;
+        for (Planet planet : planets) {
+            if (this.equals(planet)) {
+                continue;
+            }
+            sum += calcForceExertedByX(planet);
+        }
+        return sum;
+    }
+
+    public double calcNetForceExertedByY(Planet[] planets) {
+        double sum = 0.0;
+        for (Planet planet : planets) {
+            if (this.equals(planet)) {
+                continue;
+            }
+            sum += calcForceExertedByY(planet);
+        }
+        return sum;
+    }
+
+    public void update(double dt, double fx, double fy) {
+        double ax = fx / this.mass;
+        double ay = fy / this.mass;
+
+        this.xxVel += dt * ax;
+        this.yyVel += dt * ay;
+
+        this.xxPos += dt * this.xxVel;
+        this.yyPos += dt * this.yyVel;
     }
 }
