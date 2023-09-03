50c1372cef3d131128a9dd6b8cf01431fe9ac63f
yicyuanliang <gnr1118@gmail.com>
2023-09-03 16:48:38 +0800

    [project-1a] add ArrayDeque and related test code

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,141 @@
+public class ArrayDeque<T> {
+    private final T[] items;
+    private int nextFirst;
+    private int nextLast;
+    private int size;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        nextFirst = 4;
+        nextLast = 5;
+        size = 0;
+    }
+
+    public void addFirst(T item) {
+        if (size == items.length) {
+            // resize
+        }
+        items[nextFirst] = item;
+        nextFirst = indexMinusOne(nextFirst);
+        size++;
+    }
+
+    public void addLast(T item) {
+        if (size == items.length) {
+            // resize
+        }
+        items[nextLast] = item;
+        nextLast = indexPlusOne(nextLast);
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        if (isEmpty()) {
+            return;
+        }
+
+        int currIndex = indexPlusOne(nextFirst);
+
+        if (currIndex != nextLast) {
+            System.out.print(items[currIndex]);
+            currIndex = indexPlusOne(currIndex);
+        }
+
+        while (currIndex != nextLast) {
+            System.out.print(" " + items[currIndex]);
+            currIndex = indexPlusOne(currIndex);
+        }
+
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        int prevNextFirst = indexPlusOne(nextFirst);
+        T item = items[prevNextFirst];
+        nextFirst = prevNextFirst;
+        items[prevNextFirst] = null;
+        size--;
+
+        return item;
+    }
+
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        int prevNextLast = indexMinusOne(nextLast);
+        T item = items[prevNextLast];
+        nextLast = prevNextLast;
+        items[prevNextLast] = null;
+        size--;
+
+        return item;
+    }
+
+    public T get(int index) {
+        int beginIndex = indexPlusOne(nextFirst);
+        int targetIndex = (beginIndex + index) % items.length;
+        return items[targetIndex];
+    }
+
+    private int indexMinusOne(int index) {
+        index -= 1;
+        if (index < 0) {
+            index = items.length - 1;
+        }
+        return index;
+    }
+
+    private int indexPlusOne(int index) {
+        index += 1;
+        if (index > items.length - 1) {
+            index = 0;
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        System.out.println(deque.isEmpty());
+        deque.addFirst(1);
+        deque.addLast(5);
+        deque.addLast(10);
+        System.out.println(deque.isEmpty());
+        System.out.println(deque.size());
+        deque.printDeque();
+
+        System.out.println(deque.get(0) + " " + deque.get(1) + " " + deque.get(2) + " " + deque.get(3));
+
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeLast());
+        System.out.println(deque.isEmpty());
+        System.out.println(deque.size());
+        deque.printDeque();
+
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.isEmpty());
+        System.out.println(deque.size());
+        deque.printDeque();
+
+        deque.addFirst(1);
+        deque.addLast(5);
+        deque.addLast(10);
+        System.out.println(deque.isEmpty());
+        System.out.println(deque.size());
+        deque.printDeque();
+    }
+}
