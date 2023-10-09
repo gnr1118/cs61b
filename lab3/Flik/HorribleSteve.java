bb3a135f663bc93aa692d0b6b126335dc83fd689
yicyuanliang <gnr1118@gmail.com>
2023-10-09 11:58:19 +0800

    [lab3] refactor HorribleSteve

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,11 +1,10 @@
 public class HorribleSteve {
     public static void main(String [] args) {
-        int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
+        for (int i = 0, j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
-                break; // break exits the for loop!
+                System.out.println("i is " + i);
+                break;
             }
         }
-        System.out.println("i is " + i);
     }
 }
