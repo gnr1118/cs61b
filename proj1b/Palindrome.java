9af7b614e96e0bb3caf5e31b752f01b56578999d
yicyuanliang <gnr1118@gmail.com>
2023-12-16 15:58:36 +0800

    [project-1b] extract checkPalindromeIteratively helper method and add checkPalindromeRecursively helper method

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,5 +1,4 @@
 import java.util.Objects;
-import java.util.Optional;
 
 public class Palindrome {
     public Deque<Character> wordToDeque(String word) {
@@ -20,11 +19,26 @@ public class Palindrome {
         }
 
         Deque<Character> deque = wordToDeque(word);
-        for (int i = 0; i < deque.size() / 2 ;i++) {
+        return checkPalindromeIteratively(deque);
+//        return checkPalindromeRecursively(deque);
+    }
+
+    private static boolean checkPalindromeIteratively(Deque<Character> deque) {
+        for (int i = 0; i < deque.size() / 2 ; i++) {
             if (deque.removeFirst() != deque.removeLast()) {
                 return false;
             }
         }
         return true;
+    }
+
+    private static boolean checkPalindromeRecursively(Deque<Character> deque) {
+        if (deque.size() <= 1) {
+            return true;
+        }
+        if (!deque.removeFirst().equals(deque.removeLast())) {
+            return false;
+        }
+        return checkPalindromeRecursively(deque);
     }
 }
