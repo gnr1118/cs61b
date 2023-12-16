6003816babc48c080a95fdcc7101f6da60f060df
yicyuanliang <gnr1118@gmail.com>
2023-12-16 15:28:58 +0800

    [project-1b] implement wordToDeque method

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,5 +1,15 @@
+import java.util.Objects;
+import java.util.Optional;
+
 public class Palindrome {
     public Deque<Character> wordToDeque(String word) {
-        return null;
+        if (Objects.isNull(word)) {
+            return null;
+        }
+        Deque<Character> deque = new LinkedListDeque<>();
+        for (int i = 0; i < word.length() ;i++) {
+            deque.addLast(word.charAt(i));
+        }
+        return deque;
     }
 }
