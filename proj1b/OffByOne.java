4007cb6a84a652fd871e42733b2833fe7cfb20ad
yicyuanliang <gnr1118@gmail.com>
2023-12-17 10:21:52 +0800

    [project-1b] implement same char not equal

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,6 +1,10 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
+        if (x - y == 0) {
+            return false;
+        }
+
         return true;
     }
 }
